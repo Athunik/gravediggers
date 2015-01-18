4b9e1ad1fd66149a10d38c9b2dd808b8f585fed9
Athunik <athunik@reborn.com>
2015-01-19 00:59:56 +0200

    Startpage v1.0
    Startpagei algne ülesehitus.

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="tuum.css">
+	<meta charset="UTF-8" />
+	<title>Esileht</title>	
+</head>
+
+<body>
+
+<div id="navigatsioon"> 
+<nav>
+	<ul>
+		<li>
+			<div>
+				<a href="#">Torrents</a>
+			</div>
+			<ul>
+				<li><a href="https://kickass.so/">KickassTorrents</a></li>
+				<li><a href="http://www.torrentleech.org/">TorrentLeech</a></li>
+				<li><a href="http://isohunt.to/">isoHunt</a></li>
+				<li><a href="https://yts.re/">YTS</a></li>
+			</ul>
+		</li>
+		
+		<li>
+			<div>
+				<a href="#">Music</a>
+			</div>
+			<ul>
+				<li><a href="http://grooveshark.com/">Grooveshark</a></li>
+				<li><a href="https://play.spotify.com/">Spotify</a></li>
+				<li><a href="https://soundcloud.com/">SoundCloud</a></li>
+			</ul>
+		</li>
+		
+		<li>
+			<div>
+				<a href="#">Software</a>
+			</div>
+			<ul>
+				<li><a href="https://www.waterfoxproject.org/">Waterfox</a></li>
+				<li><a href="https://ninite.com/">Ninite</a></li>
+			</ul>
+		</li>
+		
+		<li>
+			<div>
+				<a href="#">Hardware</a>
+			</div>
+			<ul>
+				<li><a href="http://arvutitark.ee/">Arvutitark</a></li>
+				<li><a href="http://www.galador.ee/">Galador</a></li>
+				<li><a href="http://modikomp.ee/">Modikomp</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
+
+<hr>
+
+<nav class="nav2">
+	<ul class="ul2">
+		<li><a href="https://www.seb.ee/">S|E|B</a></li>
+		<li><a href="https://www.swedbank.ee/">Swedbank</a></li>
+		<li><a href="https://www.paypal.com/">Paypal</a></li>
+		<li><a href="http://www.ebay.com/">eBay</a></li>
+		<li><a href="http://www.aliexpress.com/">AliExpress</a></li>
+		<li><a href="http://www.amazon.com/">Amazon</a></li>
+	</ul>
+</nav>
+
+</div>
+
+<form id="otsingulahter" method="get" action="https://startpage.com/do/search">
+	<input type="text" name="q" size="100" maxlength="255" class="tekst" autocomplete="off"/>
+	<input type="submit" value="»" class="nupp"/>
+</form> 
+		
+</body>
+
+</html>
